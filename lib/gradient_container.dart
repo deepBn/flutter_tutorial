c21ab82590a760b03d2392667d88cc5334dec438
Rajdeep Barman <deepbn9@gmail.com>
2023-05-13 21:05:01 +0530

    Added and displayed images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_tutorial/styled_text.dart';
-
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -20,8 +18,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
